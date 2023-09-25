4c87bb3bc988c47866090aa2a48a20750b4bcc42
Karoline Aspegren <karolineaspegren@gmail.com>
2023-09-25 13:47:14 +0200

    Add logic to hide back button when not ChatSettings.razor

---
+++ Services/NavigationServices.cs
@@ -0,0 +1,22 @@
+﻿namespace ChatBotAI.Services;
+
+public interface INavigationServices
+{
+    bool GetChatSettingsStatus();
+    void ToggleChatSettings(bool IsChatSettings);
+}
+
+public class NavigationServices : INavigationServices
+{
+    private bool isChatSettings;
+
+    public bool GetChatSettingsStatus()
+    {
+        return isChatSettings;
+    }
+
+    public void ToggleChatSettings(bool IsChatSettings)
+    {
+        isChatSettings = IsChatSettings;
+    }
+}
