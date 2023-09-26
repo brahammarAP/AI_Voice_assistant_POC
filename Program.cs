2c5d2c65cd0853d52cba7ff9ab109e48875d536e
dalent0ft <46519397+dalent0ft@users.noreply.github.com>
2023-09-26 11:07:56 +0200

    Added Features and Fixed Bugs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using ChatBotAI.Repository.IRepository;
 using ChatBotAI.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -20,8 +19,6 @@ builder.Services.AddSingleton(builder.Configuration);
 builder.Services
     .AddSingleton<IAzureAiChatServices, AzureAiChatServices>()
     .AddSingleton<IUserSession, UserSession>();
-
-builder.Services.AddSpeechSynthesis();
 
 builder.Services
     .AddScoped<ILocalStorageAPI, LocalStorageAPIService>()
